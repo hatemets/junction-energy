403ba2604be741d57478cff81700812d618a21ca
mark <heidmetsmark@gmail.com>
2022-11-04 20:59:27 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route
+    BrowserRouter as Router,
+    Switch,
+    Route
 } from "react-router-dom";
 
 import Container from 'react-bootstrap/Container';
@@ -23,31 +23,30 @@ import DishWasher from './components/DishWasher.js';
 import WashingMachine from './components/WashingMachine.js';
 import Shower from './components/Shower.js';
 
-
 const App = () => {
-	return (
-		<Container className="p-3 main-wrapper">
+    return (
+        <Container className="p-3 main-wrapper">
 
-			<Router>
-				<Navigation />
+            <Router>
+                <Navigation />
 
-				<Switch>
-					<Route exact path="/" component={Dashboard} />
-					<Route path="/usage-times" component={UsageTimes} />
-					<Route path="/leaderboard" component={Leaderboard} />
-					<Route path="/activities" component={Activities} />
-					<Route path="/notifications" component={Notifications} />
-					<Route exact path="/faucets" component={Faucets} />
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route path="/usage-times" component={UsageTimes} />
+                    <Route path="/leaderboard" component={Leaderboard} />
+                    <Route path="/activities" component={Activities} />
+                    <Route path="/notifications" component={Notifications} />
+                    <Route exact path="/faucets" component={Faucets} />
 
-					{/* Faucet subpaths */}	
-					<Route path="/faucets/dishwasher" component={DishWasher} />
-					<Route path="/faucets/shower" component={Shower} />
-					<Route path="/faucets/washing-machine" component={WashingMachine} />
-				</Switch>
-			</Router>
+                    {/* Faucet subpaths */}	
+                    <Route path="/faucets/dishwasher" component={DishWasher} />
+                    <Route path="/faucets/shower" component={Shower} />
+                    <Route path="/faucets/washing-machine" component={WashingMachine} />
+                </Switch>
+            </Router>
 
-		</Container>
-	)
+        </Container>
+    )
 };
 
 
